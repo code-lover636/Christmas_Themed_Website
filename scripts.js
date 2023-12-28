49a7052d6f79b97e6881c673f336314e3934bcbc
Aravind Ashokan <aravindashokan636@gmail.com>
2023-12-28 13:21:55 +0530

    Animate

--- scripts.js
+++ scripts.js
@@ -1,15 +1,27 @@
 var audio = new Audio("./assets/carol.mp3#t=00:00:05");
 audio.volume = 0.2;
 
-const music = (e)=>{
+const music = (e, comp)=>{
+    console.log("Carol of Bells")
     if (audio.paused) {
         audio.play();
     } else {
         audio.pause();
     }
-    cont.children[0].classList.toggle("active");
-    cont.children[1].classList.toggle("active");
+    console.log(comp);
+    cont.forEach(child=> child.children[0].classList.toggle("active"));
+    cont.forEach(child=> child.children[1].classList.toggle("active"));
 }
 
-const cont = document.querySelector(".musicbtn")
-cont.addEventListener("click", music);
+const cont = document.querySelectorAll(".musicbtn");
+cont.forEach(e=>e.addEventListener("click", music));
+
+const gift = e=>{
+    console.log("Santa Claus");
+    const sky = document.querySelector(".nightsky");
+    sky.children[0].classList.toggle("move");
+    sky.classList.toggle("fade");
+}
+
+const giftbtn = document.querySelector(".grp");
+giftbtn.addEventListener('click', gift);
